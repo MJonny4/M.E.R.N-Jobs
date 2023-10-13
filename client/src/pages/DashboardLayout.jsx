7b7d8252800bf33174cfd063bed9dd4d94ec89a8
Ion <Ion2000m@gmail.com>
2023-10-13 11:27:56 +0200

    Loading Added

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,11 +1,18 @@
 /* eslint-disable react/prop-types */
 import Wrapper from "../assets/wrappers/Dashboard";
-import { Outlet, redirect, useLoaderData, useNavigate } from "react-router-dom";
+import {
+    Outlet,
+    redirect,
+    useLoaderData,
+    useNavigate,
+    useNavigation,
+} from "react-router-dom";
 import { BigSidebar, Navbar, SmallSidebar } from "../components";
 import { useState, createContext, useContext } from "react";
 import { checkDefaultTheme } from "../App";
 import customFetch from "../utils/customFetch";
 import { toast } from "react-toastify";
+import Loading from "../components/Loading";
 
 export const loader = async () => {
     try {
@@ -21,6 +28,9 @@ const DashboardContext = createContext();
 const DashboardLayout = () => {
     const { user } = useLoaderData();
     const navigate = useNavigate();
+
+    const navigation = useNavigation();
+    const isPageLoading = navigation.state === "loading";
 
     const [showSidebar, setShowSidebar] = useState(false);
     const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme);
@@ -60,11 +70,15 @@ const DashboardLayout = () => {
                     <div>
                         <Navbar />
                         <div className="dashboard-page">
-                            <Outlet
-                                context={{
-                                    user,
-                                }}
-                            />
+                            {isPageLoading ? (
+                                <Loading />
+                            ) : (
+                                <Outlet
+                                    context={{
+                                        user,
+                                    }}
+                                />
+                            )}
                         </div>
                     </div>
                 </main>
